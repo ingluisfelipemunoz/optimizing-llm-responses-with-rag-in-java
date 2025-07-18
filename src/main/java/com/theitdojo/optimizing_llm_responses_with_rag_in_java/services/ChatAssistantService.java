8df47af0a790065bd8c3ea367a902984629b72d6
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-07-18 17:27:19 -0400

    adding rag feature

--- src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/services/ChatAssistantService.java
+++ src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/services/ChatAssistantService.java
@@ -1,10 +1,15 @@
 package com.theitdojo.optimizing_llm_responses_with_rag_in_java.services;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -14,16 +19,22 @@ import reactor.core.publisher.Flux;
 @Service
 public class ChatAssistantService implements ChatAssistant {
     private final ChatClient chatClient;
-    private final ChatMemory chatMemory;
+    private final String glossaryContext;
+    private final PromptTemplate promptTemplate;
 
     public ChatAssistantService(ChatClient.Builder builder,
             @Value("classpath:/system-prompt.md") Resource systemPrompt,
-            ChatMemory chatMemory) {
-        this.chatMemory = chatMemory;
+            ChatMemory chatMemory,
+            @Value("classpath:/simv/glosario.txt") Resource glossaryResource,
+            @Value("classpath:/rag-prompt-template.st") Resource ragPromptTemplate) throws IOException {
+
         this.chatClient = builder
                 .defaultSystem(systemPrompt)
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
+
+        this.promptTemplate = new PromptTemplate(ragPromptTemplate);
+        this.glossaryContext = glossaryResource.getContentAsString(StandardCharsets.UTF_8);
     }
 
     @Override
@@ -47,11 +58,11 @@ public class ChatAssistantService implements ChatAssistant {
 
     @Override
     public Flux<String> askQuestionWithContext(String conversationId, String question) {
-        // TODO: Implementar la lógica de RAG en un futuro ejercicio.
-        // Cambiamos a Flux para soportar el streaming reactivo hacia la UI de Vaadin.
-        return this.chatClient.prompt()
-                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
+        Prompt prompt = this.promptTemplate.create(Map.of("context", this.glossaryContext, "question", question));
+
+        return chatClient.prompt(prompt)
                 .user(question)
+                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .stream()
                 .content();
     }
