0a141ca27cc3d4262275b5e3e440fc2bf6a2eb7e
Luis Felipe <ingluisfelipemunoz@gmail.com>
2025-07-18 16:29:30 -0400

    implementing conversation history to remember the context

--- src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/services/ChatAssistantService.java
+++ src/main/java/com/theitdojo/optimizing_llm_responses_with_rag_in_java/services/ChatAssistantService.java
@@ -3,6 +3,8 @@ package com.theitdojo.optimizing_llm_responses_with_rag_in_java.services;
 import java.util.stream.Stream;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -10,31 +12,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChatAssistantService implements ChatAssistant {
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
 
     public ChatAssistantService(ChatClient.Builder builder,
-            @Value("classpath:/system-prompt.md") Resource systemPrompt) {
-        this.chatClient = builder.defaultSystem(systemPrompt).build();
+            @Value("classpath:/system-prompt.md") Resource systemPrompt,
+            ChatMemory chatMemory) {
+        this.chatMemory = chatMemory;
+        this.chatClient = builder
+                .defaultSystem(systemPrompt)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @Override
-    public String getResponse(String message) {
+    public String getResponse(String conversationId, String message) {
         return this.chatClient.prompt()
+                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .user(message)
-                .call().content();
+                .call()
+                .content();
     }
 
     @Override
-    public Stream<String> streamResponse(String message) {
+    public Stream<String> streamResponse(String conversationId, String message) {
         return chatClient.prompt()
                 .user(message)
+                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .stream()
                 .content()
                 .toStream();
     }
 
     @Override
-    public Stream<String> askQuestionWithContext(String question) {
-        return this.streamResponse(question);
+    public Stream<String> askQuestionWithContext(String conversationId, String question) {
+        // TODO: Implementar la lógica de RAG en un futuro ejercicio.
+        return chatClient.prompt()
+                .user(question)
+                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .stream()
+                .content()
+                .toStream();
     }
 
 }
